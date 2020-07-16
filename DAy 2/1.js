ac9cef183f31fd277a01055718e58cdb7f8b00f1
rushi051 <67327859+rushi051@users.noreply.github.com>
2020-07-16 14:57:33 +0530

    Add files via upload

---
+++ DAy 2/1.js
@@ -0,0 +1,5 @@
+var str = `Please locate where 'locate' occurs!`;
+console.log(str.indexOf(`where`));
+console.log(str.lastIndexOf(`locate`));
+console.log(str.indexOf(`where`,17));
+console.log(str.search(`where`));
---
+++ DAy 2/2.js
@@ -0,0 +1,7 @@
+var cars = ["Fortuner","Endevour","Lexus","Innova"];
+console.log(cars.push("Scorpio"));
+console.log(cars);
+console.log(cars.pop("Lexus"));
+console.log(cars);
+console.log(cars.indexOf("Lexus"));
+console.log(cars.shift());
