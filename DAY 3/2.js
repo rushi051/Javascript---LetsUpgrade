9a6ba7f42fec131d63d54ef08953d6a7bfa1a20d
rushi051 <67327859+rushi051@users.noreply.github.com>
2020-07-17 14:06:54 +0530

    Add files via upload

--- DAY 3/2.js
+++ DAY 3/2.js
@@ -1,7 +1,8 @@
-var a = Number(prompt("Enter a number"));
-if (a%2==0) 
+function e ()
 {
-console.log("The number entered is",a,"and number is even");    
-} else {
-    console.log("The number entered is",a,"and number is odd");   
+    var a = Number(prompt("Enter a number"));
+    let res = a%2==0 ? "The number entered is and number is even":
+            "The number entered is and number is odd";
+    console.log(res);
 }
+console.log(e())
---
+++ DAY 3/3.js
@@ -0,0 +1,9 @@
+
+
+function two()
+{
+    var a = prompt("Enter OS name and version")
+    var r = a.split("")
+    console.log('OS name is ${r[0]} and version is ${r[1]}')
+}
+
